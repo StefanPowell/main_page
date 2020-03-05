f9648b8855c6b9f76fdce4bae04adda667cf17d0
StefanPowell <Stefan.Powell001@umb.edu>
2020-03-05 16:49:01 -0500

    update

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,11 @@
 main_div_id = "main_collage";
 popup_div_id = "pop_up";
 
+
 function pop_up(text){
 	document.getElementById(main_div_id).style.display = "none";
 	document.getElementById(popup_div_id).style.display = "block";
-	document.getElementById("heading_text").innerHTML = text;
+	document.getElementById("heading_text").innerHTML = texta;
 }
 function cancel(){
 	document.getElementById(main_div_id).style.display = "block";
