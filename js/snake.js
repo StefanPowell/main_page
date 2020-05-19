50087b1dd655b6d2e653079adb12ee0dbf8cca4f
StefanPowell <Stefan.Powell001@umb.edu>
2020-05-18 20:42:23 -0400

    update

--- js/snake.js
+++ js/snake.js
@@ -16,12 +16,14 @@ current_player = new Player(50, 0);
 block = new Block();
 
 function create_blocks(){
-  
+  //  create element
+  //  add class
+  //  add id
 }
 
 function checkpressed(x) {
     x = x || window.event;
-    if (x.keyCode == '37') {
+    if( (x.keyCode == '37') & ((parseInt(current_player.position[0], 10))>0) ){
       document.getElementById('play_er').style.display = "none";
       new_x_coord = ( parseInt(current_player.position[0], 10) - 1);
       x_new = new_x_coord.toString();
@@ -29,7 +31,7 @@ function checkpressed(x) {
       current_player.position[0] = x_new;
       document.getElementById('play_er').style.display = "block";
     }
-    else if (x.keyCode == '39') {
+    else if( (x.keyCode == '39') & ((parseInt(current_player.position[0], 10))<95) ){
       document.getElementById('play_er').style.display = "none";
       new_x_coord = ( parseInt(current_player.position[0], 10) + 1);
       x_new = new_x_coord.toString();
